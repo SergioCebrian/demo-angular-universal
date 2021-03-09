1b1f4777153223f7bd4ddeaaa8cb84ca4874cb64
SergioCebrian <sergiocebrian85@gmail.com>
2021-03-09 13:55:21 +0100

    Platform Browser

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnInit,
+  Injectable, 
+  Inject, 
+  PLATFORM_ID,
+  Optional
+} from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { Title, Meta } from '@angular/platform-browser';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { Request } from 'express';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +19,12 @@ import { Title, Meta } from '@angular/platform-browser';
 export class HomeComponent implements OnInit {
 
   constructor(
+    @Inject(PLATFORM_ID)
+    private platformID: any,
     private metaService: Meta,
-    private titleService: Title
+    private titleService: Title,
+    @Optional()
+    @Inject(REQUEST) private request: Request
   ) { }
 
   ngOnInit(): void {
@@ -20,6 +34,16 @@ export class HomeComponent implements OnInit {
       { name: 'description', content: 'Demo Angular Universal' },
       { name: 'robots', content: 'noindex, nofollow' }
     ]);
+
+    // https://www.newline.co/@eigenjoy/angular-ssr-the-browser-is-not-the-server--2b398f60
+    if (isPlatformBrowser(this.platformID)) {
+      console.log(this.platformID);
+      console.log(window.navigator.language);
+    } else {
+      console.log('server logic here');
+      let lang = (this.request.headers["accept-language"] || "").substring(0, 5);
+      console.log(lang);
+    }
   }
 
 }
